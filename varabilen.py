440d8b1602d5829ba1f617518f02adcc4a2026ec
Ghayath <ghayath94sh@gmail.com>
2022-07-30 23:26:56 +0200

    add task

---
+++ varabilen.py
@@ -0,0 +1,25 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=False
+y=9
+z=8.0
+s='5'
+float(y)
+9.0
+l=[1,2,3,4,5]
+tuple(l)
+(1, 2, 3, 4, 5)
+t=(10,11,12,13,14,15)
+d={"MO":1,"Leo":3,"CR7":7}
+# (6) yes when string is number
+#(11) yes
+#Python is interpreted
+#(13) low ->The language of the computer
+#It consists of 1 , 0
+#It is easy to be Executed
+#It is hard to program
+#high->
+#Easy to program
+#Takes less time to be coded
+#Easy to be corrected
+#Portable (only one program can run on any pc
