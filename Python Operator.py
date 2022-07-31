10bb0f49649e1da42910256ea3d1da447c4c44f9
Ghayath <ghayath94sh@gmail.com>
2022-07-31 17:18:01 +0200

    .

---
+++ Python Operator.py
@@ -0,0 +1,26 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Equal to (=) is an assignment operator, which sets the variable on the left of #the = to the value of the expression that is on its right.
+#(==) is a comparison operator, which transforms the operands having the same ty#pe before comparison.
+#(2)->that means unequal
+#(3)-> Operator precedence in Python simply refers to the order of operations. O#perators are used to perform operations on variables and values
+x=10
+x+=5
+x/5
+3.0
+x*10
+150
+x//3
+5
+print(11/4)
+2.75
+print(11//4)
+2
+print(2**3)
+8
+11/3
+3.6666666666666665
+int(11/3)
+3
+print(int(11/3))
+3
